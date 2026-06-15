d4a26fc04fec10201f36301f07ab2f02cdf671c1
Abhi0647 <abujabhijit6@gmail.com>
2026-06-15 10:44:31 +0530

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -1,9 +1,5 @@
-numbers = [10,20,30,40,50,60,70,80,90,100]
-
-highest = max(numbers)
-lowest = min(numbers)
-average = sum(numbers)/len(numbers)
-
-print("Highest:", highest)
-print("Lowest:", lowest)
-print("Average:", average)
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": "Version 3"
+    }
